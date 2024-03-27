c2fc1dfefec0754bb40faf9724f036f6054f68e3
muhammadalirahoojo <159998000+muhammadalirahoojo@users.noreply.github.com>
2024-03-27 14:08:23 +0500

    my first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+let greet = "Hello GitHub World..";
+console.log(greet);
+let userName = "Ali";
+let password = "ali_khan123";
+if (userName === "Ali" && password === "ali_khan") {
+    console.log("Congratulations! Login Successfull.");
+}
+else {
+    console.log("Invalid Username or Passwoed!");
+}
---
+++ index.ts
@@ -0,0 +1,12 @@
+let greet:string="Hello GitHub World.."
+console.log(greet);
+
+let userName:string="Ali"
+let password:string="ali_khan123"
+if (userName==="Ali" && password==="ali_khan") {
+    console.log("Congratulations! Login Successfull.");
+    
+} else {
+    console.log("Invalid Username or Password!");
+    
+}
